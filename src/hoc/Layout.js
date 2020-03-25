4e8ecf2a1d3842559c30d2a1255ab40089dc59c8
thang241190 <thangnguyen24111990@gmail.com>
2020-03-25 22:14:51 +0200

    Add sort and filter

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -1,34 +1,86 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useLayoutEffect, useCallback} from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Restaurants from "../components/Restaurants/Restaurants";
+import Sort from "../components/Sort/Sort";
 import Welcome from "../components/Welcome/Welcome";
 
 import data from "../services/restaurants.json";
 
+const sortNameAsc = variable => {
+	variable.sort((a, b) => {
+		if (a.name.toUpperCase() > b.name.toUpperCase()) {
+			return 1;
+		}
+		if (b.name.toUpperCase() > a.name.toUpperCase()) {
+			return -1;
+		}
+
+		return 0;
+	});
+};
+const sortNameDesc = variable => {
+	variable.sort((a, b) => {
+		if (a.name.toUpperCase() < b.name.toUpperCase()) {
+			return 1;
+		}
+		if (b.name.toUpperCase() < a.name.toUpperCase()) {
+			return -1;
+		}
+
+		return 0;
+	});
+};
+
 export default function Layout() {
 	const [isOnline, setIsOnline] = useState(false);
+	const [sort, setSort] = useState(null);
 	const [restaurants, setRestaurants] = useState(data.restaurants);
 
-	const handlerOnline = () => {
+	const showOnlineHandler = () => {
 		setIsOnline(!isOnline);
 	};
 
-	useEffect(() => {
+	const sortingHandler = e => {
+		setSort(e.target.value);
+	};
+
+	const loadData = () => {
+		let dataValue = restaurants;
+		const sortFunction = sortData => {
+			const newArray = [...sortData];
+			if (sort === "0") {
+				sortNameAsc(newArray);
+			} else if (sort === "1") {
+				sortNameDesc(newArray);
+			}
+			dataValue = newArray;
+		};
 		if (isOnline) {
-			setRestaurants(
-				data.restaurants.filter(
-					restaurant => restaurant.online === isOnline
-				)
+			dataValue = restaurants.filter(
+				restaurant => restaurant.online === isOnline
 			);
+			sortFunction(dataValue);
 		} else {
-			setRestaurants(data.restaurants);
+			dataValue = data.restaurants;
+			sortFunction(dataValue);
 		}
-	}, [isOnline]);
+
+		setRestaurants(dataValue);
+	};
+
+	useEffect(() => {
+		loadData();
+	}, [sort, isOnline]);
 
 	return (
 		<div>
 			<Header />
+			<Sort
+				online={isOnline}
+				showOnlineClick={showOnlineHandler}
+				sort={sortingHandler}
+			/>
 			<main>
 				<Welcome />
 				<Restaurants data={restaurants} />
